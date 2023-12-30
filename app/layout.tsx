b22564df12339bfa2c5816faed671e004d80c0a8
robertmo98 <107803618+robertmo98@users.noreply.github.com>
2023-12-30 10:33:36 +0200

    Add a markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import Navbar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,8 +25,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Theme>
-        <Navbar />
-        <main className="p-5">{children}</main>
+          <Navbar />
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
